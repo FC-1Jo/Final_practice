408f91edf8b93170810f2945c038b9af0220d2c5
TJ-Ahn <atj528@naver.com>
2023-02-06 14:21:14 +0900

    feat: Add elif to Print buzz
    Condition: i is multiple of 5.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,6 +3,8 @@ user_num = int(input("Hello! positive integer : "))
 for i in range(1, user_num+1):
     if i%3==0:
         print('fizz')
+    elif i%5==0:
+        print('buzz')
     else:
         print(f'{i}')
 
