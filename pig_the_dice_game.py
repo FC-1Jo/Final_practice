8ea3871ec363c89d1166c08a223676871c0b8fe7
doyeon9 <doyeon96@gmail.com>
2023-02-06 15:32:07 +0900

    feat: Create pig_the_dice_game
    주사위값(1~6) 설정
    주사위 값2~6나오면 총합 값 생성(turn_scoer,computer_score,player_score)
    플레이를 더 할지 말지 입력받기
    주사위 값이 1이 나오면 턴의 총합 리셋 후 차례 종료
    컴퓨터 주사위 던지기 1회 이상 던지고, 그 다음 더 던지는지에 랜덤값 부여
    총합이 100점이 먼저 넘는 족이 승리

--- pig_the_dice_game.py
+++ pig_the_dice_game.py
@@ -1,1 +1,58 @@
-print('pig the dice game')
+import random
+
+def computer_turn(computer_score, player_score):
+    turn_score = 0
+    while True:
+        dice = random.randint(1, 6)
+        if dice == 1:
+            turn_score = 0
+            print(f"Roll 1 Computer loss.  player turn!")
+            break
+        else:
+            turn_score += dice
+            print(f"Computer add {dice} now {turn_score}")
+            if turn_score + computer_score >= 100:
+                break
+            elif random.choice([True, False]):
+                computer_score+= turn_score
+                print(f"Computer {computer_score} score")
+                break
+    return turn_score
+
+def player_turn(player_score):
+    turn_score = 0
+    roll = "y"
+    while roll == "y":
+        dice = random.randint(1, 6)
+        if dice == 1:
+            turn_score = 0
+            print("Roll1 player loss. Computer turn!")
+            break
+        else:
+            turn_score += dice
+            print(f"You add {dice}. now {turn_score}")
+            if turn_score + player_score >= 100:
+             break
+            roll = input("Roll again? (y/n)")
+    player_score += turn_score
+    print(f"player  {player_score} score")
+    return turn_score
+
+def play_game():
+    player_score = 0
+    computer_score = 0
+    while player_score < 100 and computer_score < 100:
+        player_score += player_turn(player_score)
+        if player_score >= 100:
+            break
+        computer_score += computer_turn(computer_score, player_score)
+        if computer_score >= 100:
+            break
+        print(f"Player score: {player_score}, Computer score: {computer_score}")
+    if player_score >= 100:
+        print("You win!")
+    else:
+        print("Computer win!")
+
+play_game()
+
