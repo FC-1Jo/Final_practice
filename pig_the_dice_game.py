2689f7ee2a7a915e410e37b3d43e014ccc5a1b65
oorulw <minjoo1111@gmail.com>
2023-02-07 00:28:54 +0900

    feat: Add If Statement

--- pig_the_dice_game.py
+++ pig_the_dice_game.py
@@ -5,7 +5,10 @@ a = 'yes'
 while a =='yes':
     roll=random.randint(1,6)
     print(roll)
-    if roll != 1:
+    if roll == 1:
+        current = 0
+        break
+    else:
         current += roll
-        a = input("주사위를 굴리시겠습니까? yes/no: ")
+        a = input("주사위를 굴리시겠습니까? yes/no:")
 
