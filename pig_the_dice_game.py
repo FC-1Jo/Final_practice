a3bc37d2f1fd6135da4766af8cf440ab699c8fd4
oorulw <minjoo1111@gmail.com>
2023-02-06 16:00:55 +0900

    feat: Create Pig The Dice Game

--- pig_the_dice_game.py
+++ pig_the_dice_game.py
@@ -1,1 +1,10 @@
 print('pig the dice game')
+import random
+current = 0
+dice = random.randint(1,6)
+if dice != 1:
+    current += dice
+else:
+    current = 0
+print(current)
+input("roll the dice? yes / no : ")
